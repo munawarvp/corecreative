24057f56d9712bf56d3a03ce0e501b57434f695f
Ubuntu <ubuntu@ip-172-31-27-221.us-west-2.compute.internal>
2023-02-15 07:12:09 +0000

    responsiveness

--- main_part/main_part/settings.py
+++ main_part/main_part/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%4e301ae47m!)31ar4@0hcz&wo2pa(p*dq6)_#om(0g845zv(="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
