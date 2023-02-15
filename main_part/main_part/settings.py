f1b051cc59b551df4d4f339e4ca88bb420b08ed3
munawarvp <76531953+munawarvp@users.noreply.github.com>
2023-02-15 12:35:02 +0530

    Update settings.py

--- main_part/main_part/settings.py
+++ main_part/main_part/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%4e301ae47m!)31ar4@0hcz&wo2pa(p*dq6)_#om(0g845zv(="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
